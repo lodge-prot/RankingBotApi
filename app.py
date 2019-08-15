ba2a2d7b4df08987d296eb91ae4907053342dec0
ryuichi1208 <ryucrosskey@gmail.com>
2019-08-15 16:47:01 +0900

    fix app.py

--- app.py
+++ app.py
@@ -16,23 +16,22 @@ from flask import abort, jsonify, request, render_template
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-def generate_json(url_list: list, title_list: list):
+def generate_json(kind: str, url_list: list, title_list: list):
 
     ret = {}
-    ret["Qiita"] = [{}] * 30
+    ret[kind] = [{}] * len(url_list)
 
     for i, record in enumerate(list(zip(url_list, title_list))):
-        ret["Qiita"][i] = {str(i+1) : [ {"title" : record[1]}, {"url" : record[0]} ]}
+        ret[kind][i] = {str(i+1) : [ {"title" : record[1]}, {"url" : record[0]} ]}
 
     return jsonify(ret)
 
 
-def get_qiita_ranking():
-    url = "https://qiita.com/"
+def get_qiita_ranking(url):
 
     try:
         html = requests.get(url).text
-        app.logger.info('http request executed')
+        app.logger.info('http request executed to qiita')
         soup = bs(html, "html.parser")
     except:
         app.logger.warn('http request failed')
@@ -49,7 +48,28 @@ def get_qiita_ranking():
         url_list.append(article_url)
         title_list.append(tn['title'])
 
-    return generate_json(url_list, title_list)
+    return generate_json("Qiita", url_list, title_list)
+
+
+def get_hatebu_ranking(url):
+
+    try:
+        html = requests.get(url).text
+        app.logger.info('http request executed to hatebu')
+        soup = bs(html, "html.parser")
+    except:
+        app.logger.warn('http request failed')
+        abort(404)
+
+    article_list = soup.find_all("div", {"class" : "entrylist-contents-main"})
+
+    url_list, title_list = [], []
+    for article in article_list:
+        ti = article.find("a", {"class" : "js-keyboard-openable"})
+        url_list.append(ti.get("href"))
+        title_list.append(ti.get("title"))
+
+    return generate_json("hatebu", url_list, title_list)
 
 
 def local_get_qiita_ranking():
@@ -60,20 +80,29 @@ def local_get_qiita_ranking():
 
     return js
 
+
 @app.route("/api/qiita/ranking")
 def qiita():
+    url = "https://qiita.com/"
     query = request.args.get("local")
 
     if query == "1":
         app.logger.info('local executed')
         return local_get_qiita_ranking()
 
-    return get_qiita_ranking()
+    return get_qiita_ranking(url)
 
 
 @app.route("/api/qiita/ranking/local")
 def qiita_local():
-    return local_get_qiita_ranking()
+    url = "https://qiita.com/"
+    return local_get_qiita_ranking(url)
+
+
+@app.route("/api/hatebu/ranking")
+def hatebu():
+    url = "https://b.hatena.ne.jp/hotentry/it"
+    return get_hatebu_ranking(url)
 
 
 @app.errorhandler(404)
